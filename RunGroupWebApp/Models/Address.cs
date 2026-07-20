79ed1d75c104afab7bd508a97e3ee4f6a6c839df
Abdelrahman-Abouelnour <abonourabdelrahman88@gmail.com>
2026-07-20 03:27:11 +0300

    1st commit

---
+++ RunGroupWebApp/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RunGroupWebApp.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        
+    }
+}
